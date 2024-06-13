135c14145863e4f56a97531057368bae97806be5
dsalas21 <125444146+dsalas21@users.noreply.github.com>
2024-06-12 18:23:31 -0700

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -20,4 +20,37 @@ router.get('/Us', async function(req, res, next) {
   }
 });
 
+router.get('/Recolectores', async function(req, res, next) {
+  try {
+    const connection = await initializeConnection();
+    const [rows] = await connection.query("SELECT * FROM Recolectores");
+    if (rows.length === 0) {
+      return res.status(204).json({ status: 204, message: "No items found" });
+    }
+    return res.status(200).json({ status: 200, data: rows });
+    
+  } catch (err) {
+    return res.status(500).json({ status: 500, message: err.message });
+    
+  }
+});
+
+
+router.get('/Plantas', async function(req, res, next) {
+  try {
+    const connection = await initializeConnection();
+    const [rows] = await connection.query("SELECT * FROM Plantas");
+    if (rows.length === 0) {
+      return res.status(204).json({ status: 204, message: "No items found" });
+    }
+    return res.status(200).json({ status: 200, data: rows });
+  } catch (err) {
+    return res.status(500).json({ status: 500, message: err.message });
+  }
+});
+
+
+
+
+
 module.exports = router;
